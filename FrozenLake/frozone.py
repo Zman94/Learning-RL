1b7ed16bafdb05688ea498bbaae229b021736971
Zachary Gleason <zachary.gleason@ibm.com>
2019-07-19 15:52:44 -0400

    mid way thru ant algo in frozone

--- FrozenLake/frozone.py
+++ FrozenLake/frozone.py
@@ -1,7 +1,33 @@
 import gym
+import random
 env = gym.make('FrozenLake8x8-v0')
-env.reset()
-for _ in range(100):
-    env.render()
-    env.step(env.action_space.sample())
+
+explore = 1
+epsilon = .04
+gamma = .8
+hot_tub = True
+
+ant_board = [1.0]*64
+zac_board = [[0.0 for i in range(4)] for j in range(64)]
+
+for episode in range(50):
+
+    obs = env.reset()
+    while(100):
+        # env.render()
+        action = ant_action(obs)
+        obs, reward, done, info = env.step(action)
+        print(obs)
+        if done:
+            print('finished after {} steps'.format(t+1))
+            print(reward)
+            break
 env.close()
+
+def ant_action (obs, e_board, l_board):
+    moves = [up(obs)]
+
+    if epsilon > random.random():
+        return random.randrange(4)
+    else return max(moves)
+
